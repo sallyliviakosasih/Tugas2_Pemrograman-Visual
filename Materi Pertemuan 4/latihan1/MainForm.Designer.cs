86763ca40a622676ec1163f501add602791602f3
sallyliviakosasih <86108506+sallyliviakosasih@users.noreply.github.com>
2022-03-02 15:41:44 +0700

    Add files via upload

---
+++ Materi Pertemuan 4/latihan1/MainForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Sally
+ * Date: 3/2/2022
+ * Time: 2:52 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace latihan1
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(34, 66);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(158, 24);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Masukan Nama anda";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(208, 66);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(295, 22);
+			this.textBox1.TabIndex = 1;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(76, 125);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(350, 29);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Klik Saya";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(515, 438);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "Materi Pertemuan 4";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+	}
+}
